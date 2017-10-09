78ee68d2cf1d0b4de6b99412fcb592d4351b9e1f
Nafseer <nafseerk@gmail.com>
2017-10-09 16:53:50 -0400

    Added Quadratic Schedule and minor changes to annealing algorithm

--- anneal.py
+++ anneal.py
@@ -1,17 +1,21 @@
 from tsp_data import TSPData
 from tsp_helper import TSPHelper
 from tsp_state import TSPState
-from schedule import ExponentialSchedule
+from schedule import ExponentialSchedule, LogarithmicSchedule, QuadraticSchedule
 import random, math
+import time as tm
+import matplotlib.pyplot as plt
 
 class Problem(object):
 
-    def __init__(self, initialState, stoppingTemperature=5):
+    def __init__(self, initialState, stoppingTemperature=1):
         self.initialState = initialState
         self.Tstop = stoppingTemperature
         self.time = -1
         self.T = -1
         self.finalState = None
+        self.x_axis = []
+        self.y_axis = []
 
     def getStoppingTemperature(self):
         return self.Tstop
@@ -37,54 +41,65 @@ class Problem(object):
     def getFinalState(self):
         return self.finalState
 
-    def setResults(self, state, time, temperature):
+    def setResults(self, state, time, temperature, xValues, yValues):
         self.setFinalState(state)
         self.setTime(time)
         self.setTemperature(temperature)
+        self.x_axis = xValues
+        self.y_axis = yValues
         
-    
-    
 
     
 def simulated_annealing(problem, schedule):
     currentState = problem.getInitialState()
-    
-    for time in range(1, 10000):
-        print('At time %d' % time)
+    startTime = tm.time()
+    time = 1
+    x_time = [0]
+    y_cost = [currentState.getValue()]
+    while True:
+        #print('At time %d' % time)
         currentTemperature = schedule.getTemperature(time)
-        print('Current temprature = %f' % currentTemperature)
+        #print('Current temprature = %f' % currentTemperature)
 
         #TODO: When to stop
-##        #Stop when the system is at equilibrium
-##        if currentTemperature <= problem.getStoppingTemperature():
-##            problem.setResults(currentState, time, currentTemperature)
-##            return
+        #Stop when the system is at equilibrium
+        if currentTemperature <= problem.getStoppingTemperature():
+            problem.setResults(currentState, time, currentTemperature, x_time, y_cost)
+            return
 
-        print('Current state: %s' % currentState.getPath())
-        print('Current state cost = %f' % currentState.getValue())
+        #print('Current state: %s' % currentState.getPath())
+        #print('Current state cost = %f' % currentState.getValue())
         nextState = currentState.getSuccessor()
-        print('Next state: %s' % nextState.getPath())
-        print('Next state cost = %f' % nextState.getValue())
+        #print('Next state: %s' % nextState.getPath())
+        #print('Next state cost = %f' % nextState.getValue())
         deltaValue = nextState.getValue() - currentState.getValue()
-        print('deltaValue = %d' % deltaValue)
+        #print('deltaValue = %d' % deltaValue)
 
         if deltaValue < 0: #If improvement, then accept the solution  TODO: what to do if deltaValue = 0
             currentState = nextState
-            print('Better Solution...Accepting')
+            #print('Better Solution...Accepting')
         elif acceptWithProbabilty(deltaValue, currentTemperature): #If bad solution, then accept with probability
             currentState = nextState
-            print('Bad Solution...Accepting')
+            #print('Bad Solution...Accepting')
         else:
-            print('Bad Solution...Not Accepting')
+            pass
+            #print('Bad Solution...Not Accepting')
         #input('Enter to continue...')
-        
-    problem.setResults(currentState, time, currentTemperature)
+        x_time.append(time)
+        y_cost.append(currentState.getValue())
+        time += 1
+        #Stop after 5 minute    
+        currentTime = tm.time()
+        if (currentTime - startTime)/60 > 5:
+            print('Terminating after 5 minutes...')
+            break
+    problem.setResults(currentState, time, currentTemperature, x_time, y_cost)
     return
 
 
 def acceptWithProbabilty(valueChange, temperature):
     probability = math.exp(-valueChange/temperature)
-    print('P=%.2f' % probability)
+    #print('P=%.2f' % probability)
     return random.random() < probability
 
 
@@ -100,7 +115,10 @@ if __name__ == '__main__':
 
     #TODO: how to set the initial temperature
     #Create schedule
-    schedule = ExponentialSchedule(T0=50000)
+    #schedule = ExponentialSchedule()
+    #schedule = LogarithmicSchedule(T0=10000)
+    schedule = QuadraticSchedule()
+    
     
     #Perform simulated annealing
     problem = Problem(startState)
@@ -110,3 +128,5 @@ if __name__ == '__main__':
     print('The final state: %s' % finalState.getPath())
     print('The final cost: %.2f' % finalState.getValue())
     print('Solution found after %d steps and %.2f temperature' % (problem.getTime(), problem.getTemperature()))
+    plt.scatter(problem.x_axis, problem.y_axis)
+    plt.show()
--- schedule.py
+++ schedule.py
@@ -1,11 +1,12 @@
 import math
+import matplotlib.pyplot as plt
 
 class ExponentialSchedule(object):
     """Uses exponential schedule. Reference: http://iopscience.iop.org/article/10.1088/0305-4470/31/41/011/pdf 
         T(t) = T0 X αt(α raised to the power t) 
     """
 
-    def __init__(self, alpha=0.995, T0=10000):
+    def __init__(self, alpha=0.999995, T0=200000):
         self.alpha = alpha
         self.T0 = T0
         self.T = T0
@@ -18,9 +19,73 @@ class ExponentialSchedule(object):
         self.T = math.ceil(self.T*100)/100 
         return self.T
     
-    
 
+class LogarithmicSchedule(object):
+    """Uses logaritmic multiplicative schedule. Reference: Aarts, E.H.L. & Korst, J., 1989 
+        T(t) = T0 / (1 + αLog(1 + t))
+    """
+
+    def __init__(self, alpha=.995, T0=1000000):
+        self.alpha = alpha
+        self.T0 = T0
+        self.T = T0
+        
+        
+    def getTemperature(self, stepCount):
+        self.T = self.T0 / (1 + self.alpha * math.log(1 + stepCount))
+
+        #Round to 2 decimal places
+        self.T = (self.T*100)/100 
+        return self.T
+
+class QuadraticSchedule(object):
+    """Uses linear schedule. Reference: http://iopscience.iop.org/article/10.1088/0305-4470/31/41/011/pdf
+        T(t) = T0 * ((maxSteps - stepCount)/maxSteps)^4 
+    """
+    def __init__(self, alpha=1, T0=2000000, maxSteps = 2000000):
+        self.alpha = alpha
+        self.T0 = T0
+        self.T = T0
+        self.maxSteps = maxSteps
+        
+        
+    def getTemperature(self, stepCount):
+            
+        self.T = self.T0 * ((self.maxSteps - stepCount)/self.maxSteps)**4
+
+        #Round to 1 decimal places
+        self.T = (self.T*10)/10 
+        return self.T    
+
+    
 if __name__ == '__main__':
+    #Test Exponential schedule
     schedule = ExponentialSchedule()
-    for stepCount in range(1, 1001):
-        print(schedule.getTemperature(stepCount))
+    x = []
+    y = []
+    for stepCount in range(1, 1500001):
+        x.append(stepCount)
+        y.append(schedule.getTemperature(stepCount))
+    plt.scatter(x,y)
+    plt.show()
+
+##    #Test Logarithmic schedule
+##    schedule = LogarithmicSchedule()
+##    x = []
+##    y = []  
+##    for stepCount in range(1, 10001):
+##        x.append(stepCount)
+##        y.append(schedule.getTemperature(stepCount))
+##        #print(schedule.getTemperature(stepCount))
+
+    #Test Linear schedule
+    x = []
+    y = []
+    schedule = QuadraticSchedule()
+    for stepCount in range(1, 2000001):
+        x.append(stepCount)
+        y.append(schedule.getTemperature(stepCount))  
+    plt.scatter(x,y)
+    plt.show()
+
+    
